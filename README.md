730bd6dfa9094355abbbf7af67b76d3171d0c4b8
entbappy <entbappy73@gmail.com>
2024-01-03 19:00:50 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -32,3 +32,4 @@ pip install -r requirements.txt
 
 
 
+
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("My name is Bappy!")
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
